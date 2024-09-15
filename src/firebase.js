33a3bf2c55756474b03e5a06d7abc67b64389fc4
keyursolanki7071 <keyur.solanki@arsenaltech.com>
2024-09-15 14:27:37 +0530

    Firebase config file

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBN_4i3dRQF-fo9wVFLnUk1WWkSp6LpYig",
-    authDomain: "trading-journal-8005c.firebaseapp.com",
-    databaseURL: "https://trading-journal-8005c-default-rtdb.firebaseio.com",
-    projectId: "trading-journal-8005c",
-    storageBucket: "trading-journal-8005c.appspot.com",
-    messagingSenderId: "1066409474728",
-    appId: "1:1066409474728:web:850bb1a954e56e9ba4209d",
-    measurementId: "G-59J8YZ576Q"
-  };
+  //Add your firebase config   
+};
 
 export {firebaseConfig};
